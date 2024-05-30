4a7725ff7e2df7f71ed7bc9df65fd66b3cc01022
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-05-30 23:19:31 +0530

    init project

---
+++ js/style.js
@@ -0,0 +1,57 @@
+// side bar
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+    
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+
+
+//  dark mode & light mode
+const rootDoc = document.querySelector(":root");
+const toggleThemeBtn = document.querySelector("#moon-btn");
+const addDarkTheme = () => {
+  rootDoc.style.setProperty("--text-color", "#fff");
+  // rootDoc.style.setProperty("--bg-color", "#050127");
+  rootDoc.style.setProperty("--bg-color", "rgb(4, 5, 15)");
+  rootDoc.style.setProperty("--bg-off-white", "#ff8a8a");
+};
+const addLightTheme = () => {
+  rootDoc.style.setProperty("--text-color", "#000");
+  rootDoc.style.setProperty("--bg-color", "#fff");
+  rootDoc.style.setProperty("--bg-off-white", "#f1e9e7");
+};
+const darkThemeToggleBtn = () => {
+  toggleThemeBtn.classList.remove("fa-moon");
+  toggleThemeBtn.classList.add("fa-sun");
+};
+const lightThemeToggleBtn = () => {
+  toggleThemeBtn.classList.add("fa-moon");
+  toggleThemeBtn.classList.remove("fa-sun");
+};
+toggleThemeBtn.addEventListener("click", () => {
+  if (toggleThemeBtn.classList.contains("fa-moon")) {
+    darkThemeToggleBtn();
+    addDarkTheme();
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.removeItem("theme");
+    lightThemeToggleBtn();
+    addLightTheme();
+  }
+});
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme")) {
+    addDarkTheme();
+    darkThemeToggleBtn();
+  } else {
+    addLightTheme();
+    lightThemeToggleBtn();
+  }
+});
